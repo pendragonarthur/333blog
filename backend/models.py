a221792f21f22e3aa24558372cc9c7f9e432fbce
Arthur Quaresma <arthur.q@emasa.corporativo>
2025-02-13 10:08:29 -0300

    added image/video functions

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     created_by = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # image = models.ImageField(upload_to='posts/images/', blank=True, null=True) 
-    # video = models.FileField(upload_to='posts/videos/', blank=True, null=True)  
+    image = models.ImageField(upload_to='posts/images/', blank=True, null=True) 
+    video = models.FileField(upload_to='posts/videos/', blank=True, null=True)  
     def __str__(self):
         return self.title
